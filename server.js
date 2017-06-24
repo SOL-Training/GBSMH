e7259eaa5e00ada62d3bbfba4bab4c78c3d2d905
bwestover <bwestover@github.com>
2017-06-24 15:38:25 -0700

    add dotenv

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ const express = require('express');
 const bodyParser= require('body-parser')
 const app = express();
 const MongoClient = require('mongodb').MongoClient
+require('dotenv').config();
 
 var db
-MongoClient.connect('mongodb://BrettTom:Sonora6242017@cluster0-shard-00-00-tmowm.mongodb.net:27017,cluster0-shard-00-01-tmowm.mongodb.net:27017,cluster0-shard-00-02-tmowm.mongodb.net:27017/GBSMH?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin', (err, database) => {
+MongoClient.connect('mongodb://BrettTom:'+ process.env.db_password +'@cluster0-shard-00-00-tmowm.mongodb.net:27017,cluster0-shard-00-01-tmowm.mongodb.net:27017,cluster0-shard-00-02-tmowm.mongodb.net:27017/GBSMH?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin', (err, database) => {
   if (err) return console.log(err)
   db = database
   app.listen(3000, function() {
