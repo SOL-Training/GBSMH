c1de99f9bb093ebd335639facd2d9fce27839a06
SOL-Training <thuggett@sol-training.com>
2017-06-24 17:47:27 -0700

    add new field memories

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.get('/', (req, res) => {
   db.collection('quotes').find().toArray((err, result) => {
     if (err) return console.log(err)
     res.render('index.ejs', {quotes: result})
-})
+  })
 })
 
 app.post('/quotes', (req, res) => {
