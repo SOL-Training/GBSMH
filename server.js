402ec776890ba1bce48e5affe6b564497fe84477
bwestover <bwestover@github.com>
2017-06-24 17:03:44 -0700

    fix results bug

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({extended: true}))
 
 app.get('/', (req, res) => {
-  db.collection('quotes').find().toArray((err, results) => {
+  db.collection('quotes').find().toArray((err, result) => {
     if (err) return console.log(err)
     res.render('index.ejs', {quotes: result})
 })
